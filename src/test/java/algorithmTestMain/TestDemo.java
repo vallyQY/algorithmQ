5854b9e892f8d40f9c6cd408e527d6b868c4c78a
qianyong <vally_qy@126.com>
2020-01-02 17:20:35 +0800

    genericTest

--- src/test/java/algorithmTestMain/TestDemo.java
+++ src/test/java/algorithmTestMain/TestDemo.java
@@ -2,9 +2,19 @@ package algorithmTestMain;
 
 import algorithmTestDemo.ValidParentheses;
 
+
 public class TestDemo {
     public static void main(String[] args){
         ValidParentheses solution = new ValidParentheses();
+        Integer[] intArray = { 1, 2, 3, 4, 5 };
+        printArr(intArray);
         System.out.println(solution.isValidParenthese0("()"));
+    }
+
+    public static <E> void printArr(E[] arr){
+        for(E element : arr){
+            System.out.printf("%s",element);
+        }
+        System.out.println();
     }
 }
