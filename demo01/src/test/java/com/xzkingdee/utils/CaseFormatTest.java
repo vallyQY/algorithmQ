6c6e2e29b33e825bd14c2795883345e41558c063
qianyong <vally_qy@126.com>
2020-08-27 17:57:37 +0800

    【demo02 】修改驼峰转化

--- demo01/src/test/java/com/xzkingdee/utils/CaseFormatTest.java
+++ demo01/src/test/java/com/xzkingdee/utils/CaseFormatTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class CaseFormatTest {
 
     @Test
-    public void testCoverter(){
+    public void testCoverter() {
         Converter<String, String> camelConverter = CaseFormat.LOWER_CAMEL.converterTo(CaseFormat.UPPER_CAMEL);
         String input = "input_camel";
         String result = camelConverter.convert(input);
@@ -17,10 +17,18 @@ public class CaseFormatTest {
     }
 
     @Test
-    public void toTest(){
+    public void toTest() {
         /*将str按照源caseFormat格式转化为目标format格式*/
-        String result = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL,"foo-bar");
+        String result = CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, "foo-bar");
         System.out.println(result);
         assertEquals("fooBar", result);
+    }
+
+    @Test
+    public void toTestCamel() {
+        /*将str按照源caseFormat格式转化为目标format格式*/
+        String origin = "input_camel";
+        String result = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, origin);
+        System.out.println(result);
     }
 }
